0d216add9b00a86e0077afe4ef6a381b701f935a
Ovilia <zwl.sjtu@gmail.com>
2014-12-21 21:34:21 +0800

    navbar shadow when move down

--- js/ui.js
+++ js/ui.js
@@ -18,13 +18,18 @@ function indexScroll() {
         if (headerHeight - top) {
             // home section visible
             $('#header-mobile').css({
-                'margin-top': -headerMobileHeight + top * 0.2,
+                'margin-top': -headerMobileHeight + top * 0.1,
                 'opacity': 1 - ratio * ratio
             });
             $('#header-browser').css({
-                'margin-top': headerBrowserHeight + top * 0.4,
+                'margin-top': headerBrowserHeight + top * 0.2,
                 'opacity': 1 - ratio
             });
+        }
+        if (top > 0) {
+            $('.navbar').addClass('bordered');
+        } else {
+            $('.navbar').removeClass('bordered');
         }
     })
 }
